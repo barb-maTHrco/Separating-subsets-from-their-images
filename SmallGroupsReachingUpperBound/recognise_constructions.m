872f9e68f611fcc30991aa38decaa187c1a7ab32
marusalekse <marusalekse@gmail.com>
2025-11-25 12:07:12 +0100

    Create recognise_constructions.m

---
+++ SmallGroupsReachingUpperBound/recognise_constructions.m
@@ -0,0 +1,69 @@
+// Generates the permutation group of degree 2n
+// arising from the action of S_n on the cosets of A_{n-1}
+
+HCos:=function(H,S)
+  _,G:=CosetAction(H,S);
+ return G;
+end function;
+
+//
+
+SnA:=function(n)
+  S:=Sym(n);
+  S1:=Stabiliser(S,n);
+  f,X:=Action(S1,GSet(S1,{1..n-1}));
+  A:=Alt(n-1) @@ f;
+  _,G:=CosetAction(S,A);
+ return G;
+end function;
+
+// Generates the direct product H \times Sym(2) acting in the product action on 2n points
+
+C2H:=function(H);
+  n:=Degree(H);
+  gen:=Generators(H);
+  S:=Sym(2*n);
+  gen2:=[S!([i^g : i in [1..n]] cat [i^g+n : i in [1..n]]) : g in gen];
+  t:=S!([i+n : i in [1..n]] cat [1..n]);
+  return sub<S|gen2 cat [t]>;
+ end function;
+
+AH:=function(H);
+  n:=Degree(H);
+  gen:=Generators(H);
+  S:=Sym(2*n);
+  gen2:=[S!([i^g : i in [1..n]] cat [i^g+n : i in [1..n]]) : g in gen];
+  gent:=[ S!([1..j-1] cat [j+n,j+1+n] cat [j+2..n] cat [i+n : i in [1..j-1]]
+             cat [j,j+1] cat [j+2+n..2*n])    : j in [1..n-1] ];   
+  return sub<S|gen2 cat gent>;
+ end function;
+
+WrH:=function(H);
+  n:=Degree(H);
+  gen:=Generators(H);
+  S:=Sym(2*n);
+  gen2:=[S!([i^g : i in [1..n]] cat [i^g+n : i in [1..n]]) : g in gen];
+  gent:=[ S!([1..j-1] cat [j+n] cat [j+1..n] cat [i+n : i in [1..j-1]]
+             cat [j] cat [j+1+n..2*n])    : j in [1..n] ];   
+  G:=sub<S|gen2 cat gent>;
+  K:=sub<G|gent>;
+  HH:=sub<G|gen2>;
+  return G,K,HH;
+ end function;
+
+MixHSA:=function(H,S);
+  n:=Degree(H);
+  X,K,HH:=WrH(H);
+  Y:=AH(S);
+  Q,q:=quo<X|Y>;
+  cand:=[x`subgroup @@ q : x in Subgroups(Q) | #(x`subgroup) eq 2];
+  cand:=[x : x in cand | not x meet K eq K and not HH meet x eq HH];
+  if not #cand eq 1 then
+     print "ERROR:",#cand;
+  end if;
+  return cand[1];
+end function;
+
+//
+
+//
